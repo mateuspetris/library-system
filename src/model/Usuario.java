4ff5f2b18b9fdf0604af55557e8a38fb51dff4eb
Mateus Petris <mateuspetris@gmail.com>
2026-02-03 17:06:23 -0300

    Add abstract Usuario class with user management methods

---
+++ src/model/Usuario.java
@@ -0,0 +1,104 @@
+package model;
+
+import enums.TipoUsuario;
+
+import java.util.List;
+import java.util.Stack;
+
+public abstract class Usuario {
+
+    final String cpf;
+    private String nome;
+    private String email;
+    private TipoUsuario tipoUsuario;
+    private List<Emprestimo> emprestimosAtivos;
+    private Stack<Emprestimo> historicoEmprestimos;
+    private double multasPendentes;
+
+    public abstract int getLimiteEmprestimos();
+
+    public boolean podeEmprestar(){
+
+        if(multasPendentes > 0){
+            return false;
+        }
+
+        if(emprestimosAtivos.size() >= getLimiteEmprestimos()){
+            return false;
+        }
+
+        return true;
+
+    }
+
+    public void adicionarEmprestimo(Emprestimo emprestimo){
+        emprestimosAtivos.add(emprestimo);
+        historicoEmprestimos.push(emprestimo);
+
+    }
+
+    public void removerEmprestimo(Emprestimo emprestimo){
+        emprestimosAtivos.remove(emprestimo);
+    }
+
+    public Usuario(String cpf, String nome, TipoUsuario tipoUsuario, List<Emprestimo> emprestimosAtivos, Stack<Emprestimo> historicoEmprestimos, double multasPendentes) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.tipoUsuario = tipoUsuario;
+        this.emprestimosAtivos = emprestimosAtivos;
+        this.historicoEmprestimos = historicoEmprestimos;
+        this.multasPendentes = multasPendentes;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public TipoUsuario getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(TipoUsuario tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public List<Emprestimo> getEmprestimosAtivos() {
+        return emprestimosAtivos;
+    }
+
+    public void setEmprestimosAtivos(List<Emprestimo> emprestimosAtivos) {
+        this.emprestimosAtivos = emprestimosAtivos;
+    }
+
+    public Stack<Emprestimo> getHistoricoEmprestimos() {
+        return historicoEmprestimos;
+    }
+
+    public void setHistoricoEmprestimos(Stack<Emprestimo> historicoEmprestimos) {
+        this.historicoEmprestimos = historicoEmprestimos;
+    }
+
+    public double getMultasPendentes() {
+        return multasPendentes;
+    }
+
+    public void setMultasPendentes(double multasPendentes) {
+        this.multasPendentes = multasPendentes;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "cpf='" + cpf + '\'' +
+                ", nome='" + nome + '\'' +
+                ", tipoUsuario=" + tipoUsuario +
+                ", emprestimosAtivos=" + emprestimosAtivos +
+                ", historicoEmprestimos=" + historicoEmprestimos +
+                ", multasPendentes=" + multasPendentes +
+                '}';
+    }
+}
