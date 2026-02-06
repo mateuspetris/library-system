f26de7e2b9eb326a90ed7fb8daf27b61028d616e
Mateusssi <mateuspetris@gmail.com>
2026-02-05 21:37:40 -0300

    add UsuarioRepository - EmprestimoRepository

--- src/repository/EmprestimoRepository.java
+++ src/repository/EmprestimoRepository.java
@@ -1,8 +1,60 @@
 package repository;
 
+import model.Emprestimo;
+import model.Usuario;
 
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
 
 public class EmprestimoRepository {
+
+    private LinkedHashSet<Emprestimo> emprestimos;
+
+    public EmprestimoRepository() {
+        this.emprestimos = new LinkedHashSet<>();
+    }
+
+
+    public void adicionar(Emprestimo emprestimo) {
+        emprestimos.add(emprestimo);
+
+    }
+
+    public void remover(Emprestimo emprestimo){
+        emprestimos.remove(emprestimo);
+    }
+
+    public void listarTodosEmprestimos(){
+        System.out.println("\n--- Lista emprestimos---");
+        for (Emprestimo emprestimo : emprestimos){
+            System.out.println(emprestimo);
+        }
+    }
+
+    public List<Emprestimo> buscarPorUsuario(Usuario usuario){
+        List<Emprestimo> emprestimoPorUsuario = new ArrayList<>();
+        for(Emprestimo emprestimo : emprestimos){
+            if(emprestimo.usuario().equals(usuario)){
+                emprestimoPorUsuario.add(emprestimo);
+            }
+        }
+        return emprestimoPorUsuario;
+    }
+
+    public Emprestimo buscarEmprestimos(Emprestimo codigo){
+        for(Emprestimo emprestimo : emprestimos){
+            if(emprestimo.codigoEmprestimo() == codigo.codigoEmprestimo()){}
+        }
+        return null;
+    }
+
+    public boolean verificarEmprestimo(Emprestimo emprestimo){
+        return buscarEmprestimos(emprestimo) != null;
+    }
+
+
+
 
 
 
--- src/repository/UsuarioRepository.java
+++ src/repository/UsuarioRepository.java
@@ -12,5 +12,35 @@ public class UsuarioRepository {
         this.usuario = new HashSet<>();
     }
 
+    public void adicionar(Usuario usuario){
+        this.usuario.add(usuario);
+    }
+
+    public void remover(Usuario usuario){
+        this.usuario.remove(usuario);
+    }
+
+    public void listar(){
+        System.out.println("Lista de usuarios");
+        for(Usuario user : usuario){
+            System.out.println(user);
+        }
+    }
+
+    public Usuario buscarPorCpf(String cpf){
+        for (Usuario user : usuario){
+            if (user.getCpf().equals(cpf)){
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public boolean verificarCpf(String cpf){
+        return buscarPorCpf(cpf) != null;
+    }
+
+
+
 
 }
